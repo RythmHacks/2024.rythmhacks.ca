e33d289104bb81a1afa4b3260c6f2cebc8eab360
Daniel Zhang <d.zhang200788@gmail.com>
2024-03-29 20:13:37 -0400

    deps: install deps

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,13 @@
+const Landing = () => {
+    return ( 
+        <main>
+            <section>
+                <h1>
+                    Experience the <span>magic of tech</span>
+                </h1>
+            </section>
+        </main>
+     );
+}
+ 
+export default Landing;
