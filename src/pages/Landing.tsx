103935ee7206441d3738e63d4a3b0d56667ecb70
Daniel Zhang <d.zhang200788@gmail.com>
2024-03-29 23:25:22 -0400

    feat: splash page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,22 @@
+import "./Landing.scss";
+
 const Landing = () => {
-    return ( 
+    return (
         <main>
-            <section>
-                <h1>
-                    Experience the <span>magic of tech</span>
-                </h1>
+            <section className="splash flex justify-center items-center h-dvh text-center">
+                <div className="bg absolute size-full" />
+                <div className="z-10">
+                    <h1 className="text-gray-400 text-8xl font-semibold">
+                        Experience the{" "}
+                        <div className="pb-2 text-transparent bg-clip-text bg-gradient-to-r from-[#2B7FA3] from-22% to-[#C95FFB]">
+                            magic of tech
+                        </div>
+                    </h1>
+                    <h2 className="font-light text-4xl mt-6">JUNE, WATERLOO</h2>
+                </div>
             </section>
         </main>
-     );
-}
- 
+    );
+};
+
 export default Landing;
