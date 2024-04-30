122b55da75cecd3a8844980b8bcfd329425e7fac
Li Feng Yin <lifeng-yin@proton.me>
2024-04-29 20:30:40 -0400

    style: make navbar look better

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,30 +3,29 @@ import logo from '../../assets/logo.svg'
 import { useState } from 'react'
 import './Navbar.scss'
 import { FiMenu } from 'react-icons/fi'
-import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav>
+    <>
       <div className='hidden md:block'>
         <NavbarDesktop />
       </div>
       <div className='block md:hidden'>
         <NavbarMobile />
       </div>
-    </nav>
+    </>
   )
 }
 
 const NavbarDesktop = () => {
   return (
-    <nav className='border-b-[1px] border-b-[#6A6A6A] py-8 absolute top-0 w-full z-10'>
-      <div className='max-w-[70rem] mx-auto flex items-center justify-between text-xl px-8'>
-        <a className='flex items-center gap-4 text-accent' href='#'>
+    <nav className='fixed z-50 left-4 right-4 top-4 py-4 border-[1px] border-[#ffffff1f] rounded-2xl backdrop-blur-md bg-[#202020]/30'>
+      <div className='max-w-[70rem] px-8 flex items-center justify-between text-xl'>
+        <a className='flex items-center gap-4 text-accent text-2xl' href='#'>
           <img src={logo} className='w-8' />
           RythmHacks
         </a>
-        <Links 
+        <Links
           parentClass="gap-8 flex items-center"
           linkClass="text-gray-400 hover:text-gray-200 transition-colors duration-200 ease-in-out"
           key="navbar"
@@ -47,9 +46,9 @@ const NavbarMobile = () => {
 
 
   return (
-    <nav className='navbar-mobile'>
-      <div className='flex items-center justify-between text-xl w-full'>
-        <a className='flex items-center gap-4 text-accent' href='#'>
+    <nav className='navbar-mobile fixed z-50 left-4 right-4 top-4 py-4 border-[1px] border-[#ffffff1f] rounded-2xl backdrop-blur-md bg-[#202020]/30'>
+      <div className='flex items-center justify-between text-xl w-full px-8'>
+        <a className='flex items-center gap-4 text-accent text-2xl' href='#'>
           <img src={logo} className='w-8' />
           RythmHacks
         </a>
@@ -59,7 +58,7 @@ const NavbarMobile = () => {
       </div>
       <div className={`${isHamMenuOpened ? "open" : "close"} ham-menu popup`}>
         <Links 
-          parentClass="flex flex-col gap-4 bg-slate-900 p-4 rounded-md"
+          parentClass="flex flex-col gap-4 bg-zinc-900 p-4 rounded-md"
           linkClass=""
           key="navbar"
         />
